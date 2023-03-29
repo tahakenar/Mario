b4f68316ef8ba8f2079a278a6479fa8a0e70341d
Emir Kenar <emirkenar@emirs-mbp.local>
2023-03-30 02:14:32 +0300

    feat: add s pipes

--- src/Mario/Game.cpp
+++ src/Mario/Game.cpp
@@ -34,6 +34,15 @@ void Game::setSpriteTextures()
     left_pipe_sprite_.setTextureRect(sf::IntRect(0, 0, 130, 74));
     left_pipe_sprite_.setOrigin(pipe_texture_.getSize().x/2.0f, pipe_texture_.getSize().y/2.0f);
     left_pipe_sprite_.setScale(sf::Vector2f(-1,1));
+
+    right_pipe_s_sprite_.setTexture(pipe_s_texture_);
+    right_pipe_s_sprite_.setTextureRect(sf::IntRect(0,0, pipe_s_texture_.getSize().x, pipe_s_texture_.getSize().y));
+    right_pipe_s_sprite_.setOrigin(pipe_s_texture_.getSize().x/2.0f, pipe_s_texture_.getSize().y/2.0f);
+    right_pipe_s_sprite_.setScale(sf::Vector2f(-1,1));
+
+    left_pipe_s_sprite_.setTexture(pipe_s_texture_);
+    left_pipe_s_sprite_.setTextureRect(sf::IntRect(0,0, pipe_s_texture_.getSize().x, pipe_s_texture_.getSize().y));
+    left_pipe_s_sprite_.setOrigin(pipe_s_texture_.getSize().x/2.0f, pipe_s_texture_.getSize().y/2.0f);
 }
 
 void Game::setSpritePositions()
@@ -41,6 +50,8 @@ void Game::setSpritePositions()
     floor_sprite_.setPosition(sf::Vector2f(window_->getSize().x/2.0f, window_->getSize().y-floor_texture_.getSize().y));
     right_pipe_sprite_.setPosition(sf::Vector2f(window_->getSize().x-pipe_texture_.getSize().x/2.0f, window_->getSize().y-floor_texture_.getSize().y-pipe_texture_.getSize().y));
     left_pipe_sprite_.setPosition(sf::Vector2f(0+pipe_texture_.getSize().x/2.0f, window_->getSize().y-floor_texture_.getSize().y-pipe_texture_.getSize().y));
+    right_pipe_s_sprite_.setPosition(sf::Vector2f(window_->getSize().x-pipe_s_texture_.getSize().x/2.0f, 200));
+    left_pipe_s_sprite_.setPosition(sf::Vector2f(pipe_s_texture_.getSize().x/2.0f, 200));
 }
 
 void Game::update()
@@ -73,4 +84,6 @@ void Game::drawBackground(sf::RenderWindow& window)
     window.draw(floor_sprite_);
     window.draw(left_pipe_sprite_);
     window.draw(right_pipe_sprite_);
+    window.draw(right_pipe_s_sprite_);
+    window.draw(left_pipe_s_sprite_);
 }
